7968e65a7a4dbc62636db07e05f2658a1960b657
EricF6975 <eric.feuerstein@ecomail.fr>
2025-08-21 11:12:32 +0200

    ajout soustraction et message activation

--- Calculatrice.py
+++ Calculatrice.py
@@ -2,13 +2,20 @@
 def somme(a,b):
     return a+b
 
+def soustraction(a,b):
+    return a-b
+
 
 def affiche_resultat(operation, resultat):
     match operation:
         case "somme":
             print(f"La somme est {resultat}")
+        case "soustraction":
+            print(f"La différence est {resultat}")
         case _:
             print(f"Le resultat est {resultat}")
 
 
+print("Calculator activé")
 affiche_resultat("somme",somme(1,2))
+affiche_resultat("soustraction", soustraction(4,7))
